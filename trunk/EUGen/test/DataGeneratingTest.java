611878615f51319c53fe3c46c9d0247e0b1c9edb
inanc.seylan@gmail.com <inanc.seylan@gmail.com@ea4cb7bb-f8c8-3a1d-b092-b259bdf2e5e4>
2013-09-04 15:44:46 +0000

    Adding license info.

--- trunk/EUGen/test/DataGeneratingTest.java
+++ trunk/EUGen/test/DataGeneratingTest.java
@@ -1,8 +1,17 @@
-
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * This file is part of combo-obda.
+ *
+ * combo-obda is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * combo-obda is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * combo-obda. If not, see <http://www.gnu.org/licenses/>.
  */
 import edu.lehigh.swat.bench.uba.Generator;
 import java.io.File;
@@ -10,7 +19,7 @@ import org.junit.Test;
 
 /**
  *
- * @author seylan
+ * @author İnanç Seylan
  */
 public class DataGeneratingTest {
     
