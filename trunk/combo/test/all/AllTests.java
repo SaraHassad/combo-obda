10c3adde39b898209b2260a285ba5485c08bf85d
inanc.seylan <inanc.seylan@ea4cb7bb-f8c8-3a1d-b092-b259bdf2e5e4>
2014-03-13 13:56:29 +0000

    Deleted unused code related to integer intervals.

--- trunk/combo/test/all/AllTests.java
+++ trunk/combo/test/all/AllTests.java
@@ -24,7 +24,6 @@ import org.junit.runners.Suite;
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-    interval.IntervalTest.class,
     reasoning.ReasoningTestSuite.class,
     syntax.SyntaxTestSuit.class,
 })
